034e595bc83504cc668fd714a6afadfb3f7b3f62
Liam Nelski <ljnelski@gmail.com>
2020-11-16 12:44:59 -0500

    Edited Survey Module

--- server/models/survey.js
+++ server/models/survey.js
@@ -11,24 +11,22 @@ let Model = mongoose.model;
 
 let SurveySchema = Schema({
     SurveyName: String,
+    SurveyDescription: String,
     OwnerID: String, // The ID of the owner (Used for displaying the database owner in the surveyList)
     QuestionObject1: // An array of question objects    
     { 
         Question : String, // The Question being asked
         Choices: [String], // The Choices of answer
-        TotalAnswers : [Number] // The Number of times a choice has been picked
     },    
     QuestionObject2: // An array of question objects    
     { 
         QuestionBeingAsked : String, // The Question being asked
         Choices: [String], // The Choices of answer
-        TotalAnswers : [Number] // The Number of times a choice has been picked
     },
     QuestionObject3: // An array of question objects    
     { 
         QuestionBeingAsked : String, // The Question being asked
         Choices: [String], // The Choices of answer
-        TotalAnswers : [Number] // The Number of times a choice has been picked
     }    
 },
 {
