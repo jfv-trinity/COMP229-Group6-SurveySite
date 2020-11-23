c2758280ec127f35bff9bf5d4fca3e99b7c1c858
Liam Nelski <ljnelski@gmail.com>
2020-11-23 18:14:09 -0500

    trying to make this shit work

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -9,6 +9,7 @@ let express = require("express");
 let router = express.Router();
 let passport = require("passport");
 
+
 // connect the survey model
 let surveyModel = require("../models/survey");
 let Survey = surveyModel.Model; // alias
@@ -37,12 +38,14 @@ module.exports.DisplaySurveyListPage = (req, res, next) => {
     });
 };
 
-module.exports.DisplaySurveyCreatePage = (req, res, next) => {
+module.exports.DisplaySurveyCreatePage = (req, res, next) => {    
+    req.getElementById("addQuestion").addEventListener("click", AddQuestion);
     res.render("content/survey-create", {
         title: "Create a Survey",
         displayName: req.user ? req.user.displayName : "",
-        SurveyQuestions: [ [""] ]
-    });
+        SurveyQuestions: [ [""] ]           
+    });  
+    
 };
 module.exports.DisplaySurveyQuestionPage = (req, res, next) => {
     res.render("content/survey-question", {
@@ -55,38 +58,8 @@ module.exports.ProcessSurveyCreatePage = (req, res, next) => {
         SurveyName: req.body.SurveyName,
         OwnerID: req.user._id,
 
-        "QuestionObject1.Question": req.body.QuestionObject1Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject1.Choices": [
-            req.body.QuestionObject1Choice1,
-            req.body.QuestionObject1Choice2,
-            req.body.QuestionObject1Choice3,
-            req.body.QuestionObject1Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject1.TotalAnswers": [0, 0, 0, 0],
+        
 
-        "QuestionObject2.Question": req.body.QuestionObject2Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject2.Choices": [
-            req.body.QuestionObject2Choice1,
-            req.body.QuestionObject2Choice2,
-            req.body.QuestionObject2Choice3,
-            req.body.QuestionObject2Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject2.TotalAnswers": [0, 0, 0, 0],
-
-        "QuestionObject3.Question": req.body.QuestionObject3Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject3.Choices": [
-            req.body.QuestionObject3Choice1,
-            req.body.QuestionObject3Choice2,
-            req.body.QuestionObject3Choice3,
-            req.body.QuestionObject3Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject3.TotalAnswers": [0, 0, 0, 0],
     });
 
     Survey.create(newSurvey, (err, Survey) => {
@@ -98,6 +71,8 @@ module.exports.ProcessSurveyCreatePage = (req, res, next) => {
         }
     });
 };
+
+
 
 module.exports.DisplaySurveyEditPage = (req, res, next) => {
     let id = req.params.id;
@@ -181,3 +156,13 @@ module.exports.DisplaySurveyDeletePage = (req, res, next) => {
         }
     });
 };
+
+function AddQuestion()
+{
+    console.log("Adding question");
+    let question = document.getElementById("Question");
+
+    let newQuestion = question.cloneNode(true);
+    
+    document.getElementById("questions").appendChild(newQuestion);    
+}
