5a50d7eedf834e3f2a2aa343715a37728807defb
Liam Nelski <ljnelski@gmail.com>
2020-11-30 11:35:57 -0500

    Editing views to work with expiredate and IsActicve boolean

--- server/controllers/server.js
+++ server/controllers/server.js
@@ -2,142 +2,139 @@
 File name: server.js
 */
 
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let passport = require('passport');
+let passport = require("passport");
 
 // connect the user model
-let userModel = require('../models/user');
+let userModel = require("../models/user");
 let User = userModel.Model; // alias
 
 //survey model
-let surveyModel = require('../models/survey');
-let Survey = surveyModel.Model
+let surveyModel = require("../models/survey");
+let Survey = surveyModel.Model;
 
 module.exports.DisplayHomePage = (req, res, next) => {
+    Survey.find((err, surveys) => {
+        if (err) {
+            return console.error(err);
+        } 
+        else {
+            
+            let currentDate = Date.now(); // Used for checking expirery
+            let results = [];
+            for (let survey of surveys) {
+                let temp = {
+                    SurveyName: survey.SurveyName,
+                    ExpireDate: survey.ExpireDate,
+                    IsActive: survey.IsActive,
+                    _id: survey._id,
+                };
+                // Check if the the survey has expired or is not active before pushing to the displaylist
+                if (temp.ExpireDate < currentDate && IsActive) {
+                    results.push(temp);
+                }
+            }
 
-  Survey.find( (err, surveys) => {
-    if (err) {
-      return console.error(err);
-    }
-    else {
-      res.render('index',
-      { 
-        title: 'Survey Fox',
-        surveys: surveys,
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-  });
-  }
- // module.exports.DisplaySurveyListPage = (req, res, next) => {}
-module.exports.DisplayLoginPage = (req, res, next) => {
-  // check if the user is already logged in
-  if(!req.user)
-  {
-    // display login page
-    res.render('auth/login', 
-    {
-      title: 'Login',
-      messages: req.flash('loginMessage'),
-      displayName: req.user ? req.user.displayName : ''
+            res.render("index", {
+                title: "Survey Fox",
+                surveys: results,
+                displayName: req.user ? req.user.displayName : "",
+            });
+        }
     });
-  }
-  else
-  {
-    return res.redirect('/');
-  }
-}
+};
+// module.exports.DisplaySurveyListPage = (req, res, next) => {}
+module.exports.DisplayLoginPage = (req, res, next) => {
+    // check if the user is already logged in
+    if (!req.user) {
+        // display login page
+        res.render("auth/login", {
+            title: "Login",
+            messages: req.flash("loginMessage"),
+            displayName: req.user ? req.user.displayName : "",
+        });
+    } else {
+        return res.redirect("/");
+    }
+};
 
 module.exports.ProcessLoginPage = (req, res, next) => {
-  passport.authenticate('local', 
-  (err, user, info) => {
-    // server error?
-    if(err)
-    {
-      console.log(err);
-      return next(err);
-    }
+    passport.authenticate("local", (err, user, info) => {
+        // server error?
+        if (err) {
+            console.log(err);
+            return next(err);
+        }
 
-    // is there a user login error?
-    if(!user)
-    {
-      req.flash('loginMessage', 'Authentication Error');
-      return res.redirect('/login');
-    }
+        // is there a user login error?
+        if (!user) {
+            req.flash("loginMessage", "Authentication Error");
+            return res.redirect("/login");
+        }
 
-    req.logIn(user, (err) => {
-      // db server err?
-      if(err)
-      {
-        console.log(err);
-        return next(err);
-      }
+        req.logIn(user, (err) => {
+            // db server err?
+            if (err) {
+                console.log(err);
+                return next(err);
+            }
 
-      return res.redirect('/survey-list')
-    });
-  })(req, res, next);
-}
+            return res.redirect("/survey-list");
+        });
+    })(req, res, next);
+};
 
 module.exports.DisplayRegisterPage = (req, res, next) => {
-  // check if the user is not already logged in
-  if(!req.user)
-  {
-    res.render('auth/register',
-    {
-      title: 'Register',
-      messages: req.flash('registerMessage'),
-      displayName: req.user ? req.user.displayName : ''
+    // check if the user is not already logged in
+    if (!req.user) {
+        res.render("auth/register", {
+            title: "Register",
+            messages: req.flash("registerMessage"),
+            displayName: req.user ? req.user.displayName : "",
+        });
+    } else {
+        return res.redirect("/");
+    }
+};
+
+module.exports.ProcessRegisterPage = (req, res, next) => {
+    // instantiate a new  user object
+    let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        displayName: req.body.displayName,
     });
-  }
-  else
-  {
-    return res.redirect('/');
-  }
-}
 
-module.exports.ProcessRegisterPage = (req, res, next) =>
-{
-  // instantiate a new  user object
-  let newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    displayName: req.body.displayName
-  });
+    User.register(newUser, req.body.password, (err) => {
+        if (err) {
+            console.log("Error: Inserting New User");
+            if (err.name == "UserExistsError") {
+                req.flash("registerMessage", "Registration Error: an error occurred");
+                console.log("Error: Registry error");
+            }
+            return res.render("auth/register", {
+                title: "Register",
+                messages: req.flash("registerMessage"),
+                displayName: req.user ? req.user.displayName : "",
+            });
+        } else {
+            // if no error exists, the registration is successful
 
-  User.register(newUser, req.body.password, (err) => {
-    if(err)
-    {
-      console.log("Error: Inserting New User");
-      if(err.name == "UserExistsError")
-      {
-        req.flash('registerMessage', 'Registration Error: an error occurred');
-        console.log('Error: Registry error');
-      }
-      return res.render('auth/register', {
-        title: 'Register',
-        messages: req.flash('registerMessage'),
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-    else
-    {
-      // if no error exists, the registration is successful
+            // redirect the user and authenticate them
 
-      // redirect the user and authenticate them
+            // choice 1 - redirect user back to login page
 
-      // choice 1 - redirect user back to login page
+            // choice 2 - authenticate them and then redirect them
 
-      // choice 2 - authenticate them and then redirect them
+            return passport.authenticate("local")(req, res, () => {
+                res.redirect("/survey-list");
+            });
+        }
+    });
+};
 
-      return passport.authenticate('local')(req, res, () => {
-        res.redirect('/survey-list');
-      });
-    }
-  });
-}
-
-module.exports.PerformLogout = (req, res,next) => {
-  req.logout();
-  res.redirect('/');
-}
+module.exports.PerformLogout = (req, res, next) => {
+    req.logout();
+    res.redirect("/");
+};
--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -16,191 +16,203 @@ let Survey = surveyModel.Model; // alias
 //Render survey-list View
 
 module.exports.DisplaySurveyListPage = (req, res, next) => {
-    
-    let currentDate = Date.now(); // Used for checking expirery
-   
-    Survey.find({ OwnerID: req.user._id }, (err, surveys) => {
-        if (err) 
-        {
-            return console.error(err);
-        }
-        else 
-        {
-            var results = [];
-            for (var survey of surveys) 
-            {
-                var temp =
-                {
-                    SurveyName: survey.SurveyName,
-                    ExpireDate: survey.ExpireDate,
-                    IsActive: survey.IsActive,
-                    updatedAt: dateFormat(survey.updatedAt),
-                    createdAt: dateFormat(survey.createdAt),
-                    _id: survey._id
-                }
-                // Check if the the survey has expired or is not active before pushing to the displaylist
-                if (temp.ExpireDate < currentDate && IsActive)
-                {
-                    results.push(temp);
-                }
-            }
-            res.render('content/survey-list',
-                {
-                    title: 'Survey',
-                    surveys: results,
-                    displayName: req.user ? req.user.displayName : '',
-                });
-        }
-    });
-}
+
+  Survey.find({ OwnerID: req.user._id }, (err, surveys) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      let results = [];
+      for (let survey of surveys) {
+        let temp = {
+          SurveyName: survey.SurveyName,
+          ExpireDate: survey.ExpireDate,
+          IsActive: survey.IsActive,
+          updatedAt: dateFormat(survey.updatedAt),
+          createdAt: dateFormat(survey.createdAt),
+          _id: survey._id,
+        };
+
+        results.push(temp);
+      }
+      res.render("content/survey-list", {
+        title: "Survey",
+        surveys: results,
+        displayName: req.user ? req.user.displayName : "",
+      });
+    }
+  });
+};
 
 function dateFormat(msg) {
-    var date = new Date(msg)
-    var year = date.getFullYear()
-    var month = (date.getMonth() + 1).toString().padStart(2, '0')//padStart()是ES6的新方法，即设置字符串的长度，不足的用第二个参数补充
-    var day = (date.getDate()).toString().padStart(2, '0')
-    var hour = date.getHours()
-    var min = (date.getMinutes()).toString().padStart(2, '0')
-    var second = (date.getSeconds()).toString().padStart(2, '0')
-    return `${year}-${month}-${day}  ${hour}:${min}:${second}`//这个不是单引号，而是tab键上面的键
+  var date = new Date(msg);
+  var year = date.getFullYear();
+  var month = (date.getMonth() + 1).toString().padStart(2, "0"); //padStart()是ES6的新方法，即设置字符串的长度，不足的用第二个参数补充
+  var day = date.getDate().toString().padStart(2, "0");
+  var hour = date.getHours();
+  var min = date.getMinutes().toString().padStart(2, "0");
+  var second = date.getSeconds().toString().padStart(2, "0");
+  return `${year}-${month}-${day}  ${hour}:${min}:${second}`; //这个不是单引号，而是tab键上面的键
 }
 
-
 module.exports.DisplaySurveyQuestionPage = (req, res, next) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    Survey.findById(id, (err, survey) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.render("content/survey-question", {
-                title: "",
-                displayName: req.user ? req.user.displayName : "",
-                survey: survey,
-            });
-        }
-    });
+  Survey.findById(id, (err, survey) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.render("content/survey-question", {
+        title: "",
+        displayName: req.user ? req.user.displayName : "",
+        survey: survey,
+      });
+    }
+  });
 };
 
 module.exports.ProcessSurveyQuestionPage = (req, res, next) => {
-    res.redirect("/");
+  res.redirect("/");
 };
-
 
 module.exports.DisplaySurveyCreatePage = (req, res, next) => {
-    res.render("content/survey-create", {
-        title: "Create a Survey",
-        displayName: req.user ? req.user.displayName : ""
-    });
+  res.render("content/survey-create", {
+    title: "Create a Survey",
+    displayName: req.user ? req.user.displayName : "",
+  });
 };
+
 module.exports.ProcessSurveyCreatePage = (req, res, next) => {
-    let newSurvey = Survey({
-        SurveyName: req.body.SurveyName,
-        OwnerID: req.user._id
-        
-    });
+  let newSurvey = Survey({
+    SurveyName: req.body.SurveyName,
+    OwnerID: req.user._id,
 
-    Survey.create(newSurvey, (err, Survey) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            return res.redirect("/survey-list");
-        }
-    });
+    IsActive: req.body.IsActive,
+    ExpireDate: req.body.ExpireDate,
+
+    "QuestionObject1.Question": req.body.QuestionObject1Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject1.Choices": [
+      req.body.QuestionObject1Choice1,
+      req.body.QuestionObject1Choice2,
+      req.body.QuestionObject1Choice3,
+      req.body.QuestionObject1Choice4,
+    ],
+
+    "QuestionObject2.Question": req.body.QuestionObject2Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject2.Choices": [
+      req.body.QuestionObject2Choice1,
+      req.body.QuestionObject2Choice2,
+      req.body.QuestionObject2Choice3,
+      req.body.QuestionObject2Choice4,
+    ],
+
+    "QuestionObject3.Question": req.body.QuestionObject3Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject3.Choices": [
+      req.body.QuestionObject3Choice1,
+      req.body.QuestionObject3Choice2,
+      req.body.QuestionObject3Choice3,
+      req.body.QuestionObject3Choice4,
+    ],
+  });
+
+  Survey.create(newSurvey, (err, Survey) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      return res.redirect("/survey-list");
+    }
+  });
 };
-
 
 module.exports.DisplaySurveyEditPage = (req, res, next) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    Survey.findById(id, (err, survey) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.render("content/survey-edit", {
-                title: "Edit a Survey",
-                displayName: req.user ? req.user.displayName : "",
-                survey: survey,
-            });
-        }
-    });
+  Survey.findById(id, (err, survey) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.render("content/survey-edit", {
+        title: "Edit a Survey",
+        displayName: req.user ? req.user.displayName : "",
+        survey: survey,
+      });
+    }
+  });
 };
 
 //post
 module.exports.ProcessSurveyEditPage = (req, res, next) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    let updatedSurvey = Survey({
-        _id: id,
-        SurveyName: req.body.SurveyName,
-        OwnerID: req.user._id,
-        ExpireDate: req.user.ExpireDate, ////////
+  let updatedSurvey = Survey({
+    _id: id,
+    SurveyName: req.body.SurveyName,
+    OwnerID: req.user._id,
 
-        "QuestionObject1.Question": req.body.QuestionObject1Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject1.Choices": [
-            req.body.QuestionObject1Choice1,
-            req.body.QuestionObject1Choice2,
-            req.body.QuestionObject1Choice3,
-            req.body.QuestionObject1Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject1.TotalAnswers": [0, 0, 0, 0],
+    IsActive: req.body.IsActive,
+    ExpireDate: req.body.ExpireDate,
 
-        "QuestionObject2.Question": req.body.QuestionObject2Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject2.Choices": [
-            req.body.QuestionObject2Choice1,
-            req.body.QuestionObject2Choice2,
-            req.body.QuestionObject2Choice3,
-            req.body.QuestionObject2Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject2.TotalAnswers": [0, 0, 0, 0],
+    "QuestionObject1.Question": req.body.QuestionObject1Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject1.Choices": [
+      req.body.QuestionObject1Choice1,
+      req.body.QuestionObject1Choice2,
+      req.body.QuestionObject1Choice3,
+      req.body.QuestionObject1Choice4,
+    ],
 
-        "QuestionObject3.Question": req.body.QuestionObject3Question,
-        // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
-        "QuestionObject3.Choices": [
-            req.body.QuestionObject3Choice1,
-            req.body.QuestionObject3Choice2,
-            req.body.QuestionObject3Choice3,
-            req.body.QuestionObject3Choice4,
-        ],
-        // Instantiate the total Answers to 0000 as the survey is being created
-        "QuestionObject3.TotalAnswers": [0, 0, 0, 0],
-    });
+    "QuestionObject2.Question": req.body.QuestionObject2Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject2.Choices": [
+      req.body.QuestionObject2Choice1,
+      req.body.QuestionObject2Choice2,
+      req.body.QuestionObject2Choice3,
+      req.body.QuestionObject2Choice4,
+    ],
 
-    Survey.updateOne({ _id: id }, updatedSurvey, (err) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.redirect("/survey-list");
-        }
-    });
+    "QuestionObject3.Question": req.body.QuestionObject3Question,
+    // Each one of those requests is one of the choices in the array, Not sure how to pass in a array from the EJs File
+    "QuestionObject3.Choices": [
+      req.body.QuestionObject3Choice1,
+      req.body.QuestionObject3Choice2,
+      req.body.QuestionObject3Choice3,
+      req.body.QuestionObject3Choice4,
+    ]
+  });
+
+  Survey.updateOne({ _id: id }, updatedSurvey, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect("/survey-list");
+    }
+  });
 };
 
 module.exports.DisplaySurveyDeletePage = (req, res, next) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    Survey.remove({ _id: id }, (err) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.redirect("/survey-list");
-        }
-    });
+  Survey.remove({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect("/survey-list");
+    }
+  });
 };
 
 function AddQuestion() {
-    console.log("Adding question");
-    let question = document.getElementById("Question");
+  console.log("Adding question");
+  let question = document.getElementById("Question");
 
-    let newQuestion = question.cloneNode(true);
+  let newQuestion = question.cloneNode(true);
 
-    document.getElementById("questions").appendChild(newQuestion);
+  document.getElementById("questions").appendChild(newQuestion);
 }
